10009bc429a54458f9353235ee0a5b8d1145f92c
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2023-09-10 22:17:58 +0300

    Init support of memory (#4)
    Signed-off-by: Stan1slavssKy <stas00333@gmail.com>

---
+++ simulator/common/constants.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATOR_COMMON_CONSTANTS_H
+#define SIMULATOR_COMMON_CONSTANTS_H
+
+#include <cstddef>
+#include <cstdint>
+
+namespace rvsim {
+
+static constexpr size_t HWORD_SIZE = 2;
+static constexpr size_t WORD_SIZE  = 4;
+static constexpr size_t DWORD_SIZE = 8;
+
+static constexpr size_t BITS_PER_BYTE = 8;
+static constexpr size_t KBYTE_SIZE = (1 << 10);
+static constexpr size_t MBYTE_SIZE = (1 << 20);
+
+using paddr_t = uint64_t; // physical address
+using vaddr_t = uint64_t; // virtual address
+
+} // namespace rvsim
+
+#endif // SIMULATOR_COMMON_CONSTANTS_H
