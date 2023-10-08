d21b81bf62ef079e898a4482e5387c1aa9b08e8b
Hollbrok <talashkevich.da@phystech.edu>
2023-10-08 16:30:33 +0300

    add revisions to the review

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -1,25 +1,22 @@
 #include "hart/hart.h"
 #include "supervisor/supervisor.h"
 #include "memory/memory_controller.h"
-#include "argparser/parser.hpp"
+#include "argparser/parser.h"
 
 namespace rvsim {
 
 int Main(int argc, char *argv[])
 {
-    (void)argc;
-
+    auto parser = ArgParser(argc, argv);
     auto memory = PhysMemoryCtl();
     auto hart = Hart(&memory);
     auto supervisor = Supervisor(&hart, &memory);
-    auto parser = ArgParser(argc, argv);
     
     if (!parser.Parse()) {
         return EXIT_FAILURE;
-    }
-    
+    } 
   
-    const char *elf_file = argv[1];
+    std::string& elf_file = parser.GetExecFn();
     supervisor.LoadElfFile(elf_file);
 
     hart.Interpret();
